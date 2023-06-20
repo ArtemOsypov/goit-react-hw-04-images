a5130a9b76925447b7ae3720e1eaf99cd241a570
ArtemOsypov <artem.osypov@gmail.com>
2023-06-21 01:12:19 +0300

    add

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { Modal } from 'components/Modal/Modal';
+import PropTypes from 'prop-types';
+import { GalleryItem, Image } from './ImageGalleryItem.styled';
+
+export const ImageGalleryItem = ({ webformatURL, tags, largeImageURL }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+   const toggleModal = () => {
+    setIsModalOpen(prevState => !prevState);
+  };
+  
+  
+
+          return (
+      <>
+        <GalleryItem className="gallery-item">
+          <Image
+            src={webformatURL}
+            alt={tags}
+            width="500"
+            height="210"
+            loading="lazy"
+            onClick={toggleModal}
+          />
+        </GalleryItem>
+
+        {isModalOpen && (
+          <Modal
+            modalImg={largeImageURL}
+            tags={tags}
+            closeModal={toggleModal}
+          />
+        )}
+      
+      </>
+      
+    );
+  }
+  
+
+ImageGalleryItem.propTypes = {
+  webformatURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+};
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Overlay, ModalViewer, ModalImg } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export const Modal = ({ closeModal, tags, modalImg }) => {
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    const closeByEsc = e => {
+      if (e.code !== 'Escape') {
+        return;
+      }
+      closeModal();
+    };
+
+    window.addEventListener('keydown', closeByEsc);
+
+    return () => {
+      window.removeEventListener('keydown', closeByEsc);
+    };
+  }, [closeModal]);
+        
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalViewer>
+        <ModalImg src={modalImg} alt={tags} />
+      </ModalViewer>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+
+Modal.propTypes = {
+  modalImg: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  tags: PropTypes.string.isRequired,
+  // handleBackdropClick: PropTypes.func.isRequired,
+}
